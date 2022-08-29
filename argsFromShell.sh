5cd1bbfeb5d3a1d5677c84330a79acbedb9c0d33
stleye <stleye@gmail.com>
2022-08-29 13:39:49 -0300

    Added 2 scripts

---
+++ argsFromShell.sh
@@ -0,0 +1,5 @@
+#! /usr/local/bin/node
+
+var args = process.argv.slice(2);
+
+console.log(args);
---
+++ globals.sh
@@ -0,0 +1,8 @@
+#! /usr/local/bin/node
+
+console.log("__dirname: " + __dirname);
+console.log("__filename: " + __filename);
+console.log("require: " + require);
+console.log("module: " + module);
+console.log("process: " + process);
+
