4f71c084bcf7418a6a83e475f6300e6fe2ae17f1
stleye <stleye@gmail.com>
2022-08-30 10:30:30 -0300

    Renamed npm.js to app.js

--- Lesson-5/app.js
+++ Lesson-5/app.js
@@ -13,9 +13,14 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 // 
+// npm run start - we can add start to scripts in package.json 
+// npm run dev - we can  add dev to scripts in package.json
+//
+// npm uninstall <packageName> - uninstall packages
 
 
 const lodash = require('lodash');
 
 var items = [1, [2, [3, [4]]]];
 console.log(lodash.flattenDeep(items));
+console.log("Hello People");
