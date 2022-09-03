01b0d3475777dbc60503294e58b2eab60dce0447
stleye <stleye@gmail.com>
2022-09-03 13:54:41 -0300

    Moved logger to logger.js

--- Lesson-11/app.js
+++ Lesson-11/app.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
-
-const logger = (req, res, next) => {
-	const method = req.method;
-	const url = req.url;
-	const time = new Date().getFullYear()
-	console.log(method, url, time);
-	next();
-}
+const logger = require('./logger');
 
 app.get('/', logger, (req, res) => {
 	res.send('<h1>Home Page</h1>');
