ccd591467985dc3f1291750ab31189986a98c6d9
stleye <stleye@gmail.com>
2022-09-03 16:48:07 -0300

    Added morgan as a dependency and middleware

--- Lesson-11/app.js
+++ Lesson-11/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const logger = require('./logger');
+const authorize = require('./authorize');
+const morgan = require('morgan');
 
-app.use(logger);
+
+app.use(morgan('tiny'));
+//app.use([logger, authorize]);
+//app.use('/', logger) - it applies logger to the home directory
+//app.use('/api', logger) - it applies logger to all the routes that start with /api, for example /api, /api/products, /api/version, etc
 
 app.get('/', (req, res) => {
 	res.send('<h1>Home Page</h1>');
