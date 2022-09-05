3d9510d2bf581817ad887108921760021382e8c1
stleye <stleye@gmail.com>
2022-09-05 20:08:47 -0300

    Added delete

--- Lesson-13/app.js
+++ Lesson-13/app.js
@@ -36,6 +36,18 @@ app.put('/api/people/:id', (req, res) => {
   res.status(200).json({success: true, data: newPeople})
 })
 
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id) )
+  if (!person){
+    return res.status(404).json({success: false, msg: `no person with id ${id}`})
+  }
+  const newPeople = people.filter((person) => {
+    person.id !== Number(req.params.id)
+  })
+  return res.status(200).json({success: true, data: newPeople})
+  
+})
+
 app.post('/login', (req, res) => {
 	const {name} = req.body;
 	if(name) {
