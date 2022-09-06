3f466abdceb7abcad8de6f5e41972689a6fb00b8
stleye <stleye@gmail.com>
2022-09-05 21:18:54 -0300

    Code refactor using controllers

--- Lesson-14/routes/people.js
+++ Lesson-14/routes/people.js
@@ -2,45 +2,19 @@ const express = require('express')
 const router = express.Router()
 let { people } = require('../data');
 
-router.get('/', (req, res) => {
-	res.status(200).json({success: true, data: people})
-})
+const {
+  getPeople, 
+  createPerson, 
+  updatePerson, 
+  deletePerson
+} = require('../controllers/people')
 
-router.post('/', (req, res) => {
-  const {name} = req.body
-  if (!name){
-    return res.status(400).json({success: false, msg: 'please provide name vale'})
-  }
-  res.status(201).send({success: true, person: name})
-})
+router.get('/', getPeople)
 
-router.put('/:id', (req, res) => {
-  const {id} = req.params
-  const {name} = req.body
+router.post('/', createPerson)
 
-  const person = people.find((person) => person.id === Number(id) )
-  if (!person){
-    return res.status(400).json({success: false, msg: 'please provide name vale'})
-  }
-  const newPeople = people.map((person) => {
-    if(person.id === Number(id)) {
-      person.name = name
-    }
-    return person
-  })
-  res.status(200).json({success: true, data: newPeople})
-})
+router.put('/:id', updatePerson)
 
-router.delete('/:id', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id) )
-  if (!person){
-    return res.status(404).json({success: false, msg: `no person with id ${id}`})
-  }
-  const newPeople = people.filter((person) => {
-    person.id !== Number(req.params.id)
-  })
-  return res.status(200).json({success: true, data: newPeople})
-  
-})
+router.delete('/:id', deletePerson)
 
 module.exports = router;
