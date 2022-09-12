c5fd5ddced11da45e01fc369864730203f5d5b35
stleye <stleye@gmail.com>
2022-09-12 14:23:29 -0300

    Starting with TaskManager

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/hello', (req, res) => {
+  res.send("Task Manager App")
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
